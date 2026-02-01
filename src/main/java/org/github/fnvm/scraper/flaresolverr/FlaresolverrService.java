5b5557cd7c165d887886fd6125b40b061a298178
venus <vladimir.lv.mail@gmail.com>
2026-02-01 07:41:38 +0300

    add flaresolverr service

---
+++ src/main/java/org/github/fnvm/scraper/flaresolverr/FlaresolverrService.java
@@ -0,0 +1,45 @@
+package org.github.fnvm.scraper.flaresolverr;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.github.fnvm.scraper.UrlScrapingException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class FlaresolverrService {
+    private static final Logger log = LoggerFactory.getLogger(FlaresolverrService.class);
+
+    private static final String DEFAULT_FLARESOLVERR_ENDPOINT = "http://localhost:8191/v1";
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static JsonNode getResponse(ObjectNode request) throws UrlScrapingException {
+        try {
+            String requestBody = objectMapper.writeValueAsString(request);
+
+
+            HttpRequest requestToFlaresolverr = HttpRequest.newBuilder()
+                    .uri(URI.create(DEFAULT_FLARESOLVERR_ENDPOINT))
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                    .build();
+
+            try (HttpClient client = HttpClient.newHttpClient()) {
+                HttpResponse<String> response = client.send(requestToFlaresolverr, HttpResponse.BodyHandlers.ofString());
+
+                log.info("Response status: {}", response.statusCode());
+                return objectMapper.readTree(response.body());
+            }
+        } catch (IOException | InterruptedException e) {
+            log.error("Error during FlareSolverr request", e);
+            throw new UrlScrapingException("Error during FlareSolverr request");
+        }
+    }
+
+}
